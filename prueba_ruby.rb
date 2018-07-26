6ceafced93ca53d597466feb5c607f345476b6c5
max <olavemax@gmail.com>
2018-07-25 23:06:25 -0400

    second commit

--- prueba_ruby.rb
+++ prueba_ruby.rb
@@ -7,28 +7,28 @@ def listado
 end
 
 def promedios(nota)
-    file = File.open("archivo.csv", "r")
-    data = file.readlines
-    file.close
-    data.each do |values|
-      line = values.split(", ")
-      nombre = line[0]
-      arr = line[1].to_f + line[2].to_f + line[3].to_f + line[4].to_f + line[5].to_f
-      promedio = arr / 5.0
-      if promedio < nota
-      puts "Alumno #{nombre} reprobo con un promedio final #{promedio}"
-      else
-      puts "Alumno #{nombre} aprobo con un promedio final #{promedio}"
+  file = File.open("archivo.csv", "r")
+  data = file.readlines
+  file.close
+  data.each do |values|
+  line = values.split(", ")
+  nombre = line[0]
+  arr = line[1].to_f + line[2].to_f + line[3].to_f + line[4].to_f + line[5].to_f
+  promedio = arr / 5.0
+  if promedio < nota
+  puts "Alumno #{nombre} reprobo con un promedio final #{promedio}"
+  else
+  puts "Alumno #{nombre} aprobo con un promedio final #{promedio}"
      end
   end
 end
 
 
 def nuevo_archivo
-  crea_lista = promedios
+  crea_lista = listado
   File.open('nuevo_archivo.txt', 'w') do |file|
-    crea_lista.each do |nombre, promedio|
-      file.puts "#{nombre} con promedio #{promedio}"
+  crea_lista.each do |nombre, promedio|
+  file.puts "#{nombre} con promedio #{promedio}"
     end
   end
 end
@@ -37,13 +37,26 @@ end
 def inasistencias
   data = listado
   data.each do |lista|
-    nombre = lista[0]
-    asistencia = lista.select { |alu_asis| alu_asis == 'A' }.count
-    puts "Listado final de inasistencias de #{nombre} tiene #{asistencia}"
+  nombre = lista[0]
+  asistencia = lista.select { |alu_asis| alu_asis == 'A' }.count
+  puts "Listado final de inasistencias de #{nombre} es de #{asistencia}"
   end
 end
 
-
+def aprobado(nota)
+  file = File.open("archivo.csv", "r")
+  data = file.readlines
+  file.close
+  data.each do |values|
+  line = values.split(", ")
+  nombre = line[0]
+  arr = line[1].to_f + line[2].to_f + line[3].to_f + line[4].to_f + line[5].to_f
+  promedio = arr / 5.0
+  if promedio < nota
+  puts "Alumno aprobado ====>  #{nombre} "
+     end
+  end
+end
 
 # Entrada al menu de alumnos
 select = 0
@@ -66,26 +79,30 @@ puts "\n"
 puts '4.- Salir'
 puts "\n"
 
-select = gets.chomp
+select = gets.chomp.to_i
   case select
 
-  when '1'
+  when 1
           puts "\n"
           puts "Listado del promedio final de los alumnos: "
           puts "\n"
           promedios(5)
-  when '2'
+  when 2
           puts "\n"
           puts "Listado de inasistenacias de los alumnos: "
           puts "\n"
           inasistencias
-  when '3'
-        
-  when '4'
-    nuevo_archivo
-    puts ' ******* Adios *****'
+  when 3
+          puts "\n"
+          puts "Listado de alumnos aprobados: "
+          puts "\n"
+          aprobado(5)
+  when 4
+          puts ' ******* Adios *****'
   else
-    puts 'Favor elija una opcion correcta del 1 al 4'
+          puts "\n"
+          puts 'Favor elija una opcion correcta del 1 al 4'
+          puts "\n"
   end
 end
 
